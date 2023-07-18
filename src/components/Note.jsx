e28188d9f6cbfc919dd28cf0e52fa0ef8da0eb5c
Joonas Kangaskoski <72375590+joonaska@users.noreply.github.com>
2023-07-18 10:47:48 +0300

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is the note title / tässä otsikko</h1>
+      <p>This is the note content / ja tässä sisältö</p>
+    </div>
+  );
+}
+
+export default Note;
